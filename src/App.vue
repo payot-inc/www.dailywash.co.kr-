96589dc572239924e7f27d6554b037e8d9f2000c
PayOT <payot.developer@gmail.com>
2020-06-10 13:45:08 +0900

    데일리홈페이지 완료

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <div class="wrap">
+    <div class="wrap" id="home">
       <div ref="header" class="header" :class="{ ['scroll-header']: lightHeader }">
         <div class="inner">
           <div class="logo">
@@ -8,11 +8,16 @@
             <span>데일리세탁</span>
           </div>
           <div class="nav">
-            <v-btn text>Home</v-btn>
+            <v-btn text href="#home">Home</v-btn>
             <v-btn text href="#intro">데일리세탁</v-btn>
-            <v-btn text href="#intro">이용가능지역</v-btn>
-            <v-btn text href="#intro">자주묻는질문</v-btn>
-            <v-btn text href="#intro">새로운 소식</v-btn>
+            <v-btn text href="#how">이용방법</v-btn>
+            <v-btn text href="#faq">자주묻는질문</v-btn>
+            <v-btn text href="#customer">고객센터</v-btn>
+          </div>
+          <div class="nav-mobile">
+            <v-btn text @click="$refs.navSide.sideOpen(true)">
+              <v-icon >mdi-menu</v-icon>
+            </v-btn>
           </div>
         </div>
       </div>
@@ -51,7 +56,7 @@
         <div class="section02">
           <div class="inner">
             <v-row>
-              <v-col cols="4">
+              <v-col cols="12" md="4" sm="6" xs="12">
                 <div class="section-title">
                   <span class="text1">집에서 간편하게 수거배달을 <strong>예약하세요</strong></span>
                   <span class="text2">간편하게 터치 한번으로 빨래 없는 일상을 만날 수 있습니다 </span>
@@ -71,7 +76,7 @@
                   </div>
                 </div>
               </v-col>
-              <v-col cols="8">
+              <v-col cols="12" md="8" sm="6" xs="12">
                 <div class="smart">
                   <img src="@/assets/img/phone.png">
                 </div>
@@ -80,7 +85,7 @@
           </div>
         </div>
 
-        <div class="section03">
+        <div class="section03" id="how">
           <div class="inner">
             <div class="section-title">
               <span class="text1"><strong>데일리세탁</strong> 이렇게 이용해보세요?</span>
@@ -88,7 +93,7 @@
             </div>
             <div class="section-cont">
               <v-row>
-                <v-col cols="4">
+                <v-col cols="12" md="4" sm="6">
                   <dl>
                     <dt>
                       <img src="@/assets/img/how01.png">
@@ -99,7 +104,7 @@
                     </dd>
                   </dl>
                 </v-col>
-                <v-col cols="4">
+                <v-col cols="12" md="4" sm="6">
                   <dl>
                     <dt>
                       <img src="@/assets/img/how02.png">
@@ -110,7 +115,7 @@
                     </dd>
                   </dl>
                 </v-col>
-                <v-col cols="4">
+                <v-col cols="12" md="4" sm="6">
                   <dl>
                     <dt>
                       <img src="@/assets/img/how03.png">
@@ -125,11 +130,11 @@
             </div> <!-- section-cont -->
 
             <div class="search-area">
+              <label>
+                <v-icon>mdi-map-marker</v-icon>
+                <span>수거배달 가능지역 <strong>확인하기</strong></span>
+              </label>
               <div class="inBox">
-                <label>
-                  <v-icon>mdi-map-marker</v-icon>
-                  <span>수거배달 가능지역 <strong>확인하기</strong></span>
-                </label>
                 <input type="text" placeholder="검색어를 입력해주세요" />
                 <v-btn text>
                   <v-icon>mdi-magnify</v-icon>
@@ -139,47 +144,51 @@
           </div>
         </div> <!-- section03 -->
 
-        <div class="section04">
+        <div class="section04" id="faq">
           <div class="inner">
             <h4>자주묻는 질문</h4>
             <v-expansion-panels flat accordion>
               <v-expansion-panel
-                v-for="(item,i) in 5"
-                :key="i"
+                v-for="item in qna" 
+                :key="item"
               >
                 <v-expansion-panel-header>
                   <span>
-                    <label>Q</label>현재 수거배달 가능한 지역은 어디인가요?
+                    <label>Q</label>{{item.question}}
                   </span>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  현재 리화이트는 서울, 경기 수도권 및 부산, 강원, 인천, 광주, 충북, 충남 등 일부지역에서 사용가능합니다. 현재 서비스 지역이 아니라면, 앱에서 '알림'을 신청해주세요!
+                  {{item.answer}}
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
           </div>
         </div>
 
-        <div class="section05">
+        <div class="section05" id="customer">
           <div class="inner">
-            <div class="notice">
-              <div class="section-title">
-                <h4>데일리세탁</h4>
-                <h4><strong>새로운 소식</strong></h4>
-                <v-btn outlined>더 보기</v-btn>
-              </div>
-              <ul>
-                <li v-ripple>
-                  <span class="date">2020.06.09</span>
-                  <span class="text">데일리세탁은 오픈준비중입니다</span>
-                </li>
-                <li v-ripple>
-                  <span class="date">2020.06.08</span>
-                  <span class="text">게시판 테스트</span>
-                </li>
-              </ul>
+      
+            <div class="section-title">
+              <h4>데일리세탁에 대해</h4>
+              <h4><strong>아직 궁금한 점이 남으셨나요?</strong></h4>
+              <p>아래로 번호로 전화주시면 친절하게 안내드릴게요!</p>
             </div>
+            <div class="section-cont">
+              <strong>070-7807-6857</strong>
+              <p>오전10시 ~ 오후 6시 / 공휴일 휴무</p>
+            </div>
+            <!-- <ul>
+              <li v-ripple>
+                <span class="date">2020.06.09</span>
+                <span class="text">데일리세탁은 오픈준비중입니다</span>
+              </li>
+              <li v-ripple>
+                <span class="date">2020.06.08</span>
+                <span class="text">게시판 테스트</span>
+              </li>
+            </ul> -->
           </div>
+       
         </div>
       </div> <!-- contents -->
 
@@ -187,26 +196,53 @@
         <div class="logo">
           <img src="@/assets/img/logo_b.png">
         </div>
+        <div class="btns">
+          <v-btn text>데일리세탁 이용약관</v-btn>
+          <v-btn text>개인정보 취급방침</v-btn>
+        </div>
         <span>(주)페이오티 | 서비스명: 데일리세탁 | 대표: 차경진 | 사업자등록번호: 599-81-00659 |  주소: 부산광역시 금정구 부산대학로 63번길 2 과학기술연구동 201호 </span>
         <span>이메일: cs@dailywash.co.kr | 전화: 070-7807-6857 | FAX: 0504-433-6857</span>
       </div>
     </div>
+
+    <NavSide ref="navSide"/>
   </v-app>
 </template>
 
 <script>
+import NavSide from '@/components/nav-side.vue';
 
 export default {
   name: "App",
 
   components: {
-    
+    NavSide
   },
 
   data: () => ({
-    //
     lightHeader: false,
+    qna:[
+      {
+        question:'현재 수거배달 가능한 지역은 어디인가요?',
+        answer:'현재 데일리세탁은 부산 일부지역에서 사용가능합니다. 현재 서비스 지역이 아니라면, 앱에서 알림을 신청해주세요!'
+      },
+      {
+        question:'세탁가격은 어떻게 되나요?',
+        answer:'앱을 통해 예약할때 상품의 가격을 확인 할 수 있습니다'
+      },
+      {
+        question:'세탁요금 결제는 어떻게 하나요?',
+        answer:'앱 사용시 결제할 카드를 등록해주시면 예약 주문시 등록된 카드로 자동결제됩니다.'
+      },
+      {
+        question:'최소 주문금액은 얼마인가요?',
+        answer:'세탁요금은 14,900원부터 서비스를 이용 하실 수 있습니다. 하나의 상품만 주문하여도 최소 14,900원이 결제되니 주의해주세요'
+      },
+
+    ]
   }),
+
+  
 
   mounted() {
     const self = this;
@@ -234,6 +270,7 @@ export default {
   top:0px;
   left:0px;
   z-index:3;
+  
 
   .inner{
     display:flex;
@@ -254,7 +291,17 @@ export default {
       color:#fff;
       font-size:15px;
       font-weight:300;
-      margin-left:10px;
+    }
+  }
+
+  .nav-mobile{
+    display:none;
+
+    .v-btn{
+      min-width:50px;
+      border-color:#fff;
+
+      .v-icon{color:#fff;}
     }
   }
 }
@@ -271,6 +318,11 @@ export default {
         font-weight:500
       }
     }
+    .nav-mobile{
+      .v-btn{
+        .v-icon{color:#494949}
+      }
+    }
 }
 
 .inner{
@@ -280,6 +332,48 @@ export default {
   flex-direction: column;;
   justify-content: center;
   align-items: center;
+}
+
+.contents{
+  padding:0px;
+  
+  .visual{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height:800px;
+    overflow:hidden;
+    position: relative;
+
+
+    .visual-video{
+      width:100%;
+      height:400%;
+    }
+    .visual-title{
+      position:absolute;
+      text-align:center;
+      color:#fff;
+      z-index:2;
+
+      span{display:block;font-weight:400;word-break: keep-all;}
+      span.text1{letter-spacing: 20px;font-size:24px;color:#aaa;}
+      span.text2{
+        font-size:42px;
+        margin:10px 0;
+        strong{font-weight:600;}
+      }
+      span.text3{font-size:24px;color:#aaa}
+  }
+    .visual-filter{
+      position:absolute;
+      width:100%;
+      height:100%;
+      background:rgba(0,0,0,0.6);
+      background-image: url('./assets/img/visual-bg.png');
+      z-index:1;
+    }
+  }
 }
 
 .section01{
@@ -305,37 +399,12 @@ export default {
 .section02{
   display:flex;
   justify-content: center;
-  height:750px;
   background:#01a1dd url('./assets/img/section02-bg.jpg') no-repeat center / 100%;
   background-attachment: fixed;
 
   .row{padding:0px;margin:0px;}
-  .col{padding:0px;}
-
-
-  .store{
-    margin-top:50px;
-    display:flex;
-    dl{
-      display:flex;
-      flex-direction: column;
-      flex:1;
-      text-align:center;
-
-      dt{
-        width:100%;
-        height:100px;
-        background:#fff;
-        border-radius:5px;
-        padding:10px;
-        overflow: hidden;
-        img{height:100%;}
-      }
-      dd{color:#fff;margin-top:15px;}
-    }
-    dl.app-store{margin-right:10px;}
-    dl.play-store{margin-left:10px;}
-  }
+  .col-md-4{padding:0px;margin:0px;}
+  .col-sm-6{padding:0px;}
 
   .section-title{
     display:flex;
@@ -356,6 +425,33 @@ export default {
       color:rgba(255,255,255,0.8)
     }
   }
+
+  .store{
+    margin-top:50px;
+    display:flex;
+
+    dl{
+      display:flex;
+      flex-direction: column;
+      flex-basis:120px;
+      text-align:center;
+
+      dt{
+        width:100%;
+        height:70px;;
+        background:#fff;
+        border-radius:5px;
+        padding:10px;
+        overflow: hidden;
+        img{height:100%;}
+      }
+      dd{color:#fff;margin-top:15px;}
+    }
+    dl.app-store{margin-right:10px;}
+    dl.play-store{margin-left:10px;}
+  }
+
+  
   
   .smart{
     height:100%;
@@ -446,20 +542,16 @@ export default {
     height:80px;
     margin-top:140px;
     box-shadow: 5px 5px 15px rgba(0,0,0,0.05);
+    display:flex;
+    align-items: center;
 
-    .inBox{
-      width:100%;
-      display:flex;
-      align-items: center;
-      padding-left:20px;
-
-      label{
+    label{
         display:flex;
         flex-direction: row;
         align-items: center;
         border-right:1px solid #e2e2e2;
-        padding-right:20px;
         color:#01a1dd;
+        padding:0 20px;
 
         .v-icon{font-size:32px;color:#01a1dd}
         span{
@@ -467,6 +559,11 @@ export default {
           strong{color:#494949;}
         }
       }
+
+    .inBox{
+      flex:1;
+      display:flex;
+      align-items: center;
 
       input{
         flex:1;
@@ -510,102 +607,58 @@ export default {
 
 .section05{
   width:100%;
-  min-height:800px;
   display:flex;
+  min-height:600px;
   flex-direction: column;
-  background: url('./assets/img/notice-bg.png') no-repeat;
-  background-position-y: center;
-  background-position-x: 100%;
+  background: url('./assets/img/notice-bg.png') no-repeat right center;
   padding:100px 0;
 
   .inner{
-    align-items: flex-start;
-    justify-content: flex-start;
-  }
-
-  .notice{
     flex:1;
-    width:50%;
-    height:100%;
+    align-items: flex-start;
+    justify-content: center;
+  }
 
     .section-title{
       h4{font-size:36px;font-weight:400;line-height:46px}
-      .v-btn{height:30px;border:1px solid #e2e2e2;margin-top:20px;}
+      p{margin-top:20px;color:#888;font-size:16px}
     }
 
-    ul{
-      list-style-type: none;
-      padding:0px;
+    .section-cont{
       margin-top:40px;
-      width:320px;
-
-      li{
-
-        border-bottom:1px solid #e2e2e2;
-        padding:20px 0;
-        span.date{color:#01a1dd}
-        span.text{margin-top:5px;}
-        span{display:block;}
-      }
-
+      strong{font-size:36px;color:#01a1dd}
+      p{color:#888}
     }
-  }
+
+
 
   .row{padding:0;margin:0;}
   .col{padding:0;margin:0;}
 }
 
 
-.contents{
-  padding:0px;
-  
-  .visual{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    height:800px;
-    overflow:hidden;
-    position: relative;
 
-
-    .visual-video{
-      width:100%;
-      height:400%;
-    }
-    .visual-title{
-      position:absolute;
-      text-align:center;
-      color:#fff;
-      z-index:2;
-
-      span{display:block;font-weight:400;}
-      span.text1{letter-spacing: 20px;font-size:24px;color:#aaa}
-      span.text2{
-        font-size:42px;
-        margin:10px 0;
-        strong{font-weight:600;}
-      }
-      span.text3{font-size:24px;color:#aaa}
-  }
-    .visual-filter{
-      position:absolute;
-      width:100%;
-      height:100%;
-      background:rgba(0,0,0,0.6);
-      background-image: url('./assets/img/visual-bg.png');
-      z-index:1;
-    }
-  }
-}
 
 .footer{
   border-top:1px solid #e2e2e2;
   text-align: center;
-  padding:50px 0 100px 0;
+  padding:50px 20px 100px 20px;;
   
   .logo{
     margin-bottom:20px;
     img{height:50px;}
+  }
+
+  .btns{
+    margin-bottom:20px;
+    .v-btn{
+      height:30px;
+      border:1px solid #e2e2e2;
+      margin:0 5px;
+      font-size:12px;
+      font-weight:500;
+      margin-bottom:10px;
+    }
   }
 
   span{
@@ -614,6 +667,190 @@ export default {
     color:#888;
     margin-bottom:5px;
   } 
+}
+
+
+@media screen and(max-width:1200px){
+  .header{
+    padding:0px;
+    .logo{padding-left:20px;}
+    .nav{display:none}
+    .nav-mobile{
+      display:block;
+
+      .v-btn{
+        min-width:70px;
+        height:70px;
+        padding:0px;margin:0px;
+      }
+    }
+    .inner{padding:0px}
+  }
+  .inner{
+    width:100%;
+    padding:0 20px;
+  }
+
+  .contents{
+    .visual{
+      background:url('./assets/img/visual-bg.jpg') no-repeat right center;
+
+      iframe{display:none;}
+    }
+  }
+
+  .section01{
+    span.text1{font-size:32px;line-height:42px;}
+  }
+
+  .section02{
+    .smart{
+      img{width:100%;height:auto;}
+    }
+    .section-title{
+      padding:70px 0;
+      span.text1{font-size:32px;line-height:42px;}
+    }
+    .store{margin-top:20px;}
+  }
+
+  .section03{
+    .section-title{
+      span.text1{
+        font-size:32px;line-height:42px;
+        word-break: keep-all;
+      }
+    }
+    .section-cont{
+      dl{
+        margin-bottom:50px;
+        dt{
+          flex:1;
+          min-height:200px;
+          img{width:100%;}
+        }
+        dd{
+          width:100%;
+          margin-top:30px;
+          align-items: center;
+
+          label{
+            width:30px;
+            height:30px;
+            border-radius:15px;
+            margin-right:10px;
+          }
+        }
+      }
+    }
+
+    .search-area{
+      border:0px;
+      box-shadow: none;
+      flex-direction: column;
+      padding-bottom:150px;
+
+      label{
+        flex:1;
+        border-right:0px;
+        margin-bottom:20px;
+        span{}
+        strong{}
+      }
+
+      .inBox{
+        flex:1;
+        width:100%;
+        border:1px solid #e2e2e2;
+
+        input{height:60px;}
+        .v-btn{
+          min-width:60px;
+          height:60px;
+        }
+        
+      }
+    }
+  }
+
+  .section04{
+    h4{font-size:32px;}
+    .v-expansion-panel-header{
+      font-size:16px;
+    }    
+  }
+
+  .section05{
+    background: none;;
+    .section-title{
+      h4{font-size:32px;line-height:42px;word-break: break-all;}
+    }
+  }
+
+}
+
+
+@media screen and(max-width:720px){
+  .contents{
+    .visual{
+      height:720px;
+
+      .visual-title{
+        span.text1{font-size:18px;color:#aaa;letter-spacing: 10px;}
+        span.text2{
+          font-size:32px;
+          margin:10px 0;
+          strong{font-weight:600;}
+        }
+        span.text3{font-size:18px;color:#aaa}
+      }
+    }
+  }
+  
+
+  .section01{
+    img.image{
+      width:100%;
+      height:auto;
+    }
+  }
+  .section02{
+    .store{
+      margin-top:40px;
+      dl{
+        flex-basis:100px;
+        dt{height:50px;}
+      }
+    }
+  }
+
+  .section03{
+    .section-cont{
+      dl{
+        dt{
+          width:80%;
+        }
+        dd{width:80%}
+      }
+    }
+  }
+
+  .section05{
+    .section-cont{
+      strong{font-size:28px;}
+      p{font-size:14px;}
+    }
+  }
+
+  .section04{
+    .v-expansion-panel-header{
+      font-size:14px;
+      line-height:21px;
+    }
+    .v-expansion-panel-content{
+      margin:0 -10px;
+    }
+  }
 }
 
 </style>
---
+++ src/components/nav-side.vue
@@ -0,0 +1,83 @@
+<template>
+
+  <div class="nav-side" v-show="sideState">
+    <div class="logo">
+      <img src="@/assets/img/logo_b.png">
+    </div>
+    <div class="menu">
+      <v-btn text href="#home" @click="sideOpen(false)">Home</v-btn>
+      <v-btn text href="#intro" @click="sideOpen(false)">데일리세탁</v-btn>
+      <v-btn text href="#how" @click="sideOpen(false)">이용방법</v-btn>
+      <v-btn text href="#faq" @click="sideOpen(false)">자주묻는 질문</v-btn>
+      <v-btn text href="#customer" @click="sideOpen(false)">고객센터</v-btn>
+    </div>
+    <v-btn class="close" text @click="sideOpen(false)">
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sideState:false,
+    }
+  },
+  methods: {
+    sideOpen(item){
+      this.sideState = item
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.nav-side{
+  font-family:'SCDream';
+  position:fixed;
+  width:240px;
+  height:100%;
+  right:0px;
+  background: #fff;
+  z-index:3;
+  border-left:1px solid #e2e2e2;
+
+  .logo{
+    border-bottom:1px solid #e2e2e2;
+    padding:30px 50px;
+    img{width:100%;}
+  }
+
+  .menu{
+    display:flex;
+    flex-direction: column;
+    margin-top:20px;
+
+    .v-btn{
+      border-radius:0px;
+      justify-content: flex-start;
+      height:50px;
+      font-weight:400;
+    }
+  }
+
+  .v-btn.close{
+    position:absolute;
+    top:0px;
+    right:0px;
+    min-width:50px;
+    height:50px;
+    border-radius:50%;
+    padding:0px;
+    
+  }
+}
+
+@media screen and(min-width:1200px){
+  .nav-side{display:none;}
+}
+
+</style>
